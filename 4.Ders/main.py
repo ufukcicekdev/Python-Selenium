2db5400a64c4f652c3407902a84a6ddd8d0ef3f3
EAGLEEARTH <ufukcicek199@gmail.com>
2023-03-18 23:42:23 +0300

    4.Ders Eklendi

---
+++ 4.Ders/main.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get("https://www.google.com.tr/?hl=tr")
+sleep(5)
+input = driver.find_element(By.NAME,"q")
+input.send_keys("kodalamaio")
+searchButton = driver.find_element(By.NAME,"btnK")
+sleep(2)
+searchButton.click()
+sleep(2)
+firstResultt = driver.find_element(By.XPATH,"/html/body/div[7]/div/div[11]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/a")
+firstResultt.click()
+
+listOfCourses = driver.find_elements(By.CLASS_NAME,"course-listing")
+print(f"Kodalama io sitesinde {len(listOfCourses)} sayısında kurs var")
+
+while True:
+    continue
+
+
+
+#
+#//*[@id="rso"]/div[1]/div/div/div/div/div/div/div[1]/a
+# HTTML LOCATORS
